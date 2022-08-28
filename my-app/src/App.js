0930fd95ae4b34faa4b9194629fe970ebb54bcb9
gianramirez <radicgian@gmail.com>
2022-08-27 21:01:44 -0400

    added pics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import AppRouter from './components/app-router';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './components/footer';
+import Header from './components/header';
 import logo from './logo.svg';
 
 function App() {
   return (
     <BrowserRouter>
+        <Header />
             <AppRouter />
           <Footer />  
         </BrowserRouter>
