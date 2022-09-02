4609a83f78a460fb2af3a78dad1ecc414fc4c994
gianramirez <radicgian@gmail.com>
2022-09-02 12:05:44 -0400

    added foundational code to menu, contact pages

---
+++ my-app/src/pages/contact-screen/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+const ContactScreen = () => {
+
+    return (
+
+    )
+}
+
+export default ContactScreen;
---
+++ my-app/src/pages/menu-screen/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MenuScreen = () => {
+  return (
+
+  )  
+}
+
+export default MenuScreen;
